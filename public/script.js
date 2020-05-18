5a7b75bbe96d1b437dd9491437656e294fde9fd6
Glitch (w-shirt) <none>
2020-05-18 19:54:52 +0000

    getting better

--- public/script.js
+++ public/script.js
@@ -1,15 +1,28 @@
-const { ko } = window;
+const //imports
+  { ko, hello } = window;
 
 console.clear();
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const colors = ["darkblue", "green", "red", "gray", "blue"];
+//https://www.jeans-fritz.de/regular-fit-hemd-mit-kleinem-karo-0110709.html?farbpalette=86&catId=64
+const colors = ["RoyalBlue", "DarkSeaGreen", "tomato", "LightGrey", "DodgerBlue"];
 
 //Tage zwischen
 const daysBetween = (one, another) => {
   return Math.round(Math.abs(+one - +another) / 8.64e7);
+};
+
+//getWeek to connect real to fake
+Date.prototype.getWeekNumber = function() {
+  var d = new Date(
+    Date.UTC(this.getFullYear(), this.getMonth(), this.getDate())
+  );
+  var dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
 };
 
 //Kombinationen
@@ -27,78 +40,85 @@ const perm = xs => {
       }
     }
   }
-  return ret.sort(function() {
-    return 0.5 - Math.random();
-  });
+  return ret;
 };
 
 const permData = perm(colors);
 
+//fake year lol
 const $tymes = [];
-Array.from(Array(52 * 7)).forEach((x, i) => {
-  $tymes.push({
-    tday: i + 1,
-    wday: (i % 7) + 1,
-    week: Math.floor(i / 7) + 1,
-    month: Math.floor(i / 28) + 1,
-    mday: i % 28 === 0 ? 28 : i % 28
+{
+  Array.from(Array(52 * 7)).forEach((x, i) => {
+    $tymes.push({
+      day: i + 1,
+      wday: (i % 7) + 1,
+      week: Math.floor(i / 7) + 1,
+      month: Math.floor(i / 28) + 1,
+      mday: i % 28 === 0 ? 1 : (i % 28) + 1
+    });
   });
-});
+}
 
 const tModel = {
   tyme: ko.observable($tymes[0]),
   combos: permData.length,
-  color: ko.observable("gold")
-};
-
-Date.prototype.dayOfYear = function() {
-  var j1 = new Date(this);
-  j1.setMonth(0, 0);
-  return Math.round((this - j1) / 8.64e7);
-};
-
-const updateUI = oldVal => {
-  const $day = tModel.day;
-  oldVal = +oldVal;
-
-  const clicked = $("input[type=radio]:focus") !== null;
-
-  setTimeout(() => {
-    const newWeekday = $day.getDay() === 0 ? 7 : $day.getDay();
-
-    const chk = $$("input")[newWeekday - 1];
-    console.log(chk);
-    chk && [(chk.checked = true)];
-  });
+  shirtColor: ko.observable("gold")
 };
 
 const updateByClick = newWeekday => {
   const $tyme = tModel.tyme;
 
-  if (newWeekday > $tyme().day) {
-    console.debug("up");
-  }
-
   //update tyme based on new weekday
   $tyme($tymes.find(x => x.wday === newWeekday && x.week === $tyme().week));
+};
 
-  tModel.color(permData[$tyme().week-1][$tyme().wday-1]);
+const jumpFromInput = function(obj, event) {
+  const $tyme = tModel.tyme;
+
+  $tyme($tymes.find(x => x.day === +event.srcElement.value));
 };
 
 //reflect color updates
-tModel.color.subscribe(newVal => {
-  
-
-  
+tModel.shirtColor.subscribe(newVal => {
   const $tyme = tModel.tyme;
-  
- 
-  
-  
-  if ($tyme().wday > 5) [tModel.color("???"), (newVal = "gold")];
 
-  $("#color>val").style.color = newVal;
+  if ($tyme().wday > 5) {
+    tModel.shirtColor("???"), (newVal = "white");
+
+    for (const stroke of $$("svg#shirt>g>path")) {
+      //color shirt black
+      stroke.style.stroke = "black";
+      //update colorname
+      $("#color>val").style.color = "black";
+    }
+  } else {
+    //apply default stroke to outline (white)
+    for (const stroke of $$("svg#shirt>g>path")) {
+      stroke.style.stroke = "white";
+    }
+
+    //update pants
+    for (const stroke of $$("svg#pants>g>path")) {
+      stroke.style.fill = $tyme().day % 2 === 0 ? "navy" : "black";
+    }
+
+    //update colorname
+    $("#color>val").style.color = newVal;
+  }
+
+  //color shirt
   $("#shirt>g>path#main").style.fill = newVal;
+});
+
+//reflect day updates on tyme var
+tModel.tyme.subscribe(newVal => {
+  const $tyme = tModel.tyme;
+
+  tModel.shirtColor(permData[$tyme().week - 1][$tyme().wday - 1]);
+
+  //init checkbox
+  const chk = $$("input")[$tyme().wday - 1];
+  chk && [(chk.checked = true)];
 });
 
 for (const _input of $$("input[type=radio]")) {
@@ -132,15 +152,14 @@ ko.applyBindings(tModel);
       stroke.style.stroke = "white";
     }
 
-    //init checkbox
-    const chk = $$("input")[$tyme().wday];
-    chk && [(chk.checked = true)];
-
-
-    
- 
-    
-    //init color <todo
-    tModel.color(permData[$tyme().week-1][$tyme().day-1]);
+    //let's begin
+    const now = new Date();
+    $tyme(
+      $tymes.find(
+        x =>
+          x.wday === (now.getDay() === 0 ? 7 : now.getDay()) &&
+          x.week === now.getWeekNumber()
+      )
+    );
   }
 }
